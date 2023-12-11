a8d211091cbf96d333fb64055218f2988ebd7d40
Oluwadhara <olufemioluwadara@gmail.com>
2023-12-11 02:38:11 +0100

    App prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -79,7 +79,7 @@ async function start() {
     const port = process.env.PORT || 8000;
   
     app.listen(port, () => {
-      console.log('Server is listening on port ' +port);
+      console.log('Server is listening on port ' + port);
     });
   }
   
